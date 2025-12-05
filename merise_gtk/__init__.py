7d35887c7315baa1033722c17dd8ad1a6b7fbc67
kheldae <taho.gougeon@gmail.com>
2025-12-05 12:01:27 +0100

    base structure, missing gresource support

--- merise_gtk/__init__.py
+++ merise_gtk/__init__.py
@@ -1,4 +1,5 @@
 import gi
+import os
 
 # package metadata
 __version__ = "0.0.0"
@@ -6,24 +7,19 @@ __version__ = "0.0.0"
 # GTK and Adwaita checks
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
-from gi.repository import Gtk, Adw
+gi.require_version("GIRepository", "3.0")
+from gi.repository import Adw, Gio, Gtk
+
+# TODO load GResource data
+resource = Gio.Resource.load("merise_gtk.gresource")
+resource._register()
+
+# TODO inner imports
+from .application import MGTKApp
 
 
-class MyApp(Adw.Application):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.connect("activate", self.on_activate)
-
-    def on_activate(self, app) -> None:
-        builder = Gtk.Builder()
-        builder.add_from_file("merise_gtk/merise_gtk.ui")
-        # display main window
-        self.win = builder.get_object("root-window")
-        self.win.set_application(self)
-        self.win.present()
-
-
+# app launcher
 def main() -> None:
-    app = MyApp(application_id="test")
+    app: MGTKApp = MGTKApp(
+        application_id="com/github/merisedot-dev/merise_gtk")
     app.run()
---
+++ merise_gtk/application.py
@@ -0,0 +1,30 @@
+import gi
+
+# GTK checks
+from gi.repository import Adw, Gio, Gtk
+
+# inner imports
+
+
+class MGTKApp(Adw.ApplicationWindow):
+    # TODO fetch relevant components
+    # TODO fetch relevant signals
+    # TODO fetch relevant properties
+
+    def __init__(self, **kwargs) -> None:
+        self._proj = None # TODO define project type
+
+        # GTK constructors like to do stuff
+        super().__init__(kwargs)
+
+        # link correct behavior
+        self.connect("activate", self.on_activate)
+
+    # launch method
+    def on_activate(self, app) -> None:
+        builder = Gtk.Builder()
+        builder.add_from_file("merise_gtk/application.ui")
+        # display main window
+        self.win = builder.get_object("root-window")
+        self.win.set_application(self)
+        self.win.present()

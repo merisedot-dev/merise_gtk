0335a33213f66bc90d5697a9aa585a4dbe7166a6
kheldae <taho.gougeon@gmail.com>
2025-12-14 21:55:05 +0100

    trimming

--- merise_gtk/__init__.py
+++ merise_gtk/__init__.py
@@ -1,12 +1,5 @@
-import gi
-
 # package metadata
 __version__ = "0.0.0"
-
-# GTK and Adwaita checks
-gi.require_version("Gtk", "4.0")
-gi.require_version("Adw", "1")
-from gi.repository import Adw, Gtk
 
 # inner imports
 from .application import MGTKApp
